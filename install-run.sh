e3fc0847215e6550c1b79a252a603bf8b5ee02b6
Yannick <yannick.spreen@gmail.com>
2025-02-14 15:59:16 -0500

    Fix rotation.

--- install-run.sh
+++ install-run.sh
@@ -5,6 +5,4 @@ mkdir -p /Applications/heic.app/Contents/MacOS
 cp .build/release/heic /Applications/heic.app/Contents/MacOS/
 
 # Launch the agent
-/Applications/heic.app/Contents/MacOS/heic >/dev/null 2>&1 &
-
-echo Done.
+/Applications/heic.app/Contents/MacOS/heic
