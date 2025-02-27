66bd66358acfa865c532a7a434d907dbba1ca8d7
Yannick <yannick.spreen@gmail.com>
2025-02-27 13:35:31 -0500

    Delete on install.

--- clone.sh
+++ clone.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # /bin/bash -c "$(curl -L https://raw.githubusercontent.com/yspreen/heic-converter/refs/heads/main/clone.sh)"
+# /bin/bash -c "$(curl -L https://raw.githubusercontent.com/yspreen/heic-converter/refs/heads/next/clone.sh)"
 
 cd /tmp
 
--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 swift build -c release
 
+rm -rf /Applications/heic.app
+
 # Create app bundle structure
 mkdir -p /Applications/heic.app/Contents/MacOS
 cp .build/release/heic /Applications/heic.app/Contents/MacOS/
