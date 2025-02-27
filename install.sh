5bd8d67438f9e64bd2c1d14b948c44cef53be58c
Yannick <yannick.spreen@gmail.com>
2025-02-27 15:38:24 -0500

    Make date check more lenient.

--- install.sh
+++ install.sh
@@ -1,16 +1,15 @@
 swift build -c release
 
-ps | grep /Applications/heic.app | grep -v grep | grep -Eo '^[0-9]+' | while read pid; do kill $pid; done
 rm -rf /Applications/heic.app
 
 # Create app bundle structure
 mkdir -p /Applications/heic.app/Contents/MacOS
-echo "Build contents:"
-ls .build/release/
+
 cp .build/release/heic /Applications/heic.app/Contents/MacOS/
 cp -r .build/release/heic_heic.bundle /Applications/heic.app/heic_heic.bundle
 
 # Launch the agent
-/Applications/heic.app/Contents/MacOS/heic >/dev/null 2>&1 &
+ps -A | grep /Applications/heic.app | grep -v grep | grep -Eo '^[0-9]+' | while read pid; do kill $pid; done
+/Applications/heic.app/Contents/MacOS/heic &
 
 echo Done. Uninstall by deleting \`/Applications/heic.app\`
