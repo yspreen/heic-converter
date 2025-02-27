612650d998a47cbed58a50ec6b010004a79bbc7b
Yannick <yannick.spreen@gmail.com>
2025-02-27 13:40:22 -0500

    Fix copy.

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ mkdir -p /Applications/heic.app/Contents/MacOS
 echo "Build contents:"
 ls .build/release/
 cp .build/release/heic /Applications/heic.app/Contents/MacOS/
-cp .build/release/heic_heic.bundle /Applications/heic.app/heic_heic.bundle
+cp -r .build/release/heic_heic.bundle /Applications/heic.app/heic_heic.bundle
 
 # Launch the agent
 /Applications/heic.app/Contents/MacOS/heic >/dev/null 2>&1 &
